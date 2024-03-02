c53e0221e4e6f0cec415b753c6d7b0ea0554ec87
jjs0211 <lovemia1224@gmail.com>
2024-03-02 22:40:50 +0900

    DB스키마 생성

--- spring-batch/src/main/java/io/springbatch/springbatch/SpringBatchApplication.java
+++ spring-batch/src/main/java/io/springbatch/springbatch/SpringBatchApplication.java
@@ -1,12 +1,18 @@
 package io.springbatch.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * EnableBatchProcessing 스프링배치활성화 어노테이션으로 선언이 필요함.
+ */
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBatchApplication.class, args);
 	}
 
